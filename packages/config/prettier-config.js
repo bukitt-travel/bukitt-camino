2dd58bdaca217d24cd3c60225b3e550c98c16627
Luis Anaya <brothersailorwebdev@gmail.com>
2022-06-30 13:41:13 +0200

    trying to get eslint fixed

--- packages/config/prettier-config.js
+++ packages/config/prettier-config.js
@@ -1,8 +1,8 @@
 module.exports = {
-  tabWidth: 4,
-  printWidth: 100,
-  singleQuote: true,
-  trailingComma: "all",
-  bracketSpacing: true,
-  plugins: [require("prettier-plugin-tailwindcss")],
+	tabWidth: 4,
+	printWidth: 100,
+	singleQuote: true,
+	trailingComma: "all",
+	bracketSpacing: true,
+	plugins: [require("prettier-plugin-tailwindcss")],
 };
